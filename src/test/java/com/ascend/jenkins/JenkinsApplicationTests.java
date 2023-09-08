392785d981008b82245ab1bd6968b915bd5dd186
Arun S <arun.sreekantan@walmart.com>
2023-09-08 12:33:46 +0530

    First Commit

---
+++ src/test/java/com/ascend/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascend.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
